e8ffe8dfcc1afc36ca8d29fd7e8f26907d01d828
Andrew <andrewhu@uw.edu>
2017-08-01 08:21:43 -0700

    Added Node.js server to the same directory as Apache
    under the node/ folder

--- node/test.js
+++ node/test.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
+  //res.status(200);
   res.writeHead(200);
   res.end('Hello Http');
 });
 server.listen(8080);
+console.log("server initialized");
